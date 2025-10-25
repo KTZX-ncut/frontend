a66f38befd690a4057e250050584ff30be555f71
liuyangjuncong20202570 <318135748@qq.com>
2025-10-25 17:18:25 +0800

    fix:import depedency

--- apps/main-project/src/views/page/stuQues/index.vue
+++ apps/main-project/src/views/page/stuQues/index.vue
@@ -2,8 +2,13 @@
   <div class="task-list">
     <Header title="问卷列表" :pathData="pathData" />
     <!-- 作业列表 -->
-    <el-table ref="multipleTableRef" class="custom-table" :data="tableData" style="width: 100%;margin-top: 15px;"
-      @selection-change="handleSelectionChange">
+    <el-table
+      ref="multipleTableRef"
+      class="custom-table"
+      :data="tableData"
+      style="width: 100%; margin-top: 15px"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column type="index" label="序号" width="80" />
       <el-table-column property="name" label="名称" />
       <el-table-column property="createTime" label="上传时间" />
@@ -14,29 +19,38 @@
       </el-table-column>
       <el-table-column fixed="right" property="address" label="操作">
         <template #default="scope">
-          <el-button 
-            v-if="scope.row.status === 0" 
+          <el-button
+            v-if="scope.row.status === 0"
             class="custom-link-button custom-link-active-button"
-            @click="(() => {
-            router.push({
-              path: '/homes/studentcourses/exam/detail',
-              query: {
-                id: scope.row.id,
-                type: 'edit'
+            @click="
+              () => {
+                router.push({
+                  path: '/homes/studentcourses/exam/detail',
+                  query: {
+                    id: scope.row.id,
+                    type: 'edit'
+                  }
+                });
               }
-            })
-          })">
+            "
+          >
             去做问卷
           </el-button>
-          <el-button v-else class="custom-link-button custom-link-active-button" @click="(() => {
-            router.push({
-              path: '/homes/studentcourses/exam/detail',
-              query: {
-                id: scope.row.id,
-                type: 'view'
+          <el-button
+            v-else
+            class="custom-link-button custom-link-active-button"
+            @click="
+              () => {
+                router.push({
+                  path: '/homes/studentcourses/exam/detail',
+                  query: {
+                    id: scope.row.id,
+                    type: 'view'
+                  }
+                });
               }
-            })
-          })">
+            "
+          >
             查看
           </el-button>
         </template>
@@ -46,55 +60,60 @@
       <!-- <el-pagination v-model:currentPage="params.pageIndex" v-model:page-size="params.pageSize"
         :page-sizes="[10, 20, 30, 40]" layout="total, sizes, prev, pager, next, jumper" :total="total"
         @size-change="handleSizeChange" @current-change="handleCurrentChange" /> -->
-        <Pagination :pageIndex="params.pageIndex" :pageSize="params.pageSize" :total="total" @update:pageIndex="handleCurrentChange" />
+      <Pagination
+        :pageIndex="params.pageIndex"
+        :pageSize="params.pageSize"
+        :total="total"
+        @update:pageIndex="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
-import Pagination from "@/views/page/components/pagination/index.vue";
-import { ref, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-import { ElTable } from 'element-plus'
-import { myQueFormPager } from '@/api/stuQues.js'
-import Header from '@/views/page/components/header/index.vue'
-const router = useRouter()
-const { currentRoute } = router
-const route = currentRoute.value
-const tableData = ref([])
-const total = ref(0)
+import Pagination from '@/views/page/components/pagination/index.vue';
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { ElTable } from 'element-plus';
+import { myQueFormPager } from '@/api/stuQues.js';
+import Header from '@/views/page/components/header/index.vue';
+const router = useRouter();
+const { currentRoute } = router;
+const route = currentRoute.value;
+const tableData = ref([]);
+const total = ref(0);
 const params = ref({
   pageIndex: 1,
   pageSize: 20
-})
+});
 const pathData = [
   {
     name: '问卷列表',
     path: ''
   }
-]
+];
 onMounted(() => {
-  getMyQueFormPager()
-})
+  getMyQueFormPager();
+});
 
-const handleSizeChange = (val) => {
-  params.value.pageSize = val
-  getMyQueFormPager()
-}
+const handleSizeChange = val => {
+  params.value.pageSize = val;
+  getMyQueFormPager();
+};
 
-const handleCurrentChange = (val) => {
-  params.value.pageIndex = val
-  getMyQueFormPager()
-}
+const handleCurrentChange = val => {
+  params.value.pageIndex = val;
+  getMyQueFormPager();
+};
 
 const getMyQueFormPager = () => {
   myQueFormPager(params.value).then(res => {
     if (res.code === '200') {
-      tableData.value = res.data.data
-      total.value = res?.data?.recordSize ?? 0
+      tableData.value = res.data.data;
+      total.value = res?.data?.recordSize ?? 0;
     }
-  })
-}
+  });
+};
 </script>
 
 <style scoped>
